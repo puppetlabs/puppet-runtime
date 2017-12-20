978d9d70f03f1f45d97471eb0ac5e3eeb2bde3e8
Michael Smith <michael.smith@puppet.com>
2017-12-19 16:23:24 -0800

    (maint) Add libicu component
    Initially intended for use in pe-client-tools.

---
+++ resources/files/libicu/zone_reslocal.mk
@@ -0,0 +1,38 @@
+# © 2016 and later: Unicode, Inc. and others.
+# License & terms of use: http://www.unicode.org/copyright.html#License
+ZONE_CLDR_VERSION = 31.0.1
+# A list of txt's to build
+# This file was created by copying data/zone/resfiles.mk from the icu data
+# source, and removing unused locales.
+
+# Aliases without a corresponding xx.xml file (see icu-config.xml & build.xml)
+ZONE_SYNTHETIC_ALIAS = en_NH.txt en_RH.txt ja_JP.txt ja_JP_TRADITIONAL.txt
+
+
+# All aliases (to not be included under 'installed'), but not including root.
+ZONE_ALIAS_SOURCE = $(ZONE_SYNTHETIC_ALIAS)
+
+
+# Ordinary resources
+ZONE_SOURCE = en.txt en_001.txt\
+ en_150.txt en_AG.txt en_AI.txt en_AT.txt en_AU.txt\
+ en_BB.txt en_BE.txt en_BM.txt en_BS.txt en_BW.txt\
+ en_BZ.txt en_CA.txt en_CC.txt en_CH.txt en_CK.txt\
+ en_CM.txt en_CX.txt en_CY.txt en_DE.txt en_DG.txt\
+ en_DK.txt en_DM.txt en_ER.txt en_FI.txt en_FJ.txt\
+ en_FK.txt en_FM.txt en_GB.txt en_GD.txt en_GG.txt\
+ en_GH.txt en_GI.txt en_GM.txt en_GU.txt en_GY.txt\
+ en_HK.txt en_IE.txt en_IL.txt en_IM.txt en_IN.txt\
+ en_IO.txt en_JE.txt en_JM.txt en_KE.txt en_KI.txt\
+ en_KN.txt en_KY.txt en_LC.txt en_LR.txt en_LS.txt\
+ en_MG.txt en_MH.txt en_MO.txt en_MP.txt en_MS.txt\
+ en_MT.txt en_MU.txt en_MW.txt en_MY.txt en_NA.txt\
+ en_NF.txt en_NG.txt en_NL.txt en_NR.txt en_NU.txt\
+ en_NZ.txt en_PG.txt en_PH.txt en_PK.txt en_PN.txt\
+ en_PW.txt en_RW.txt en_SB.txt en_SC.txt en_SD.txt\
+ en_SE.txt en_SG.txt en_SH.txt en_SI.txt en_SL.txt\
+ en_SS.txt en_SX.txt en_SZ.txt en_TC.txt en_TK.txt\
+ en_TO.txt en_TT.txt en_TV.txt en_TZ.txt en_UG.txt\
+ en_VC.txt en_VG.txt en_VU.txt en_WS.txt en_ZA.txt\
+ en_ZM.txt en_ZW.txt eo.txt\
+ ja.txt jgo.txt jmc.txt ka.txt kab.txt
