d14a206a0a2769964b636ca793aa453ca03b6820
Casey Williams <caseywilliams@users.noreply.github.com>
2018-01-23 16:06:32 -0800

    (maint) Bump fast_gettext gem version

--- configs/components/rubygem-fast_gettext.rb
+++ configs/components/rubygem-fast_gettext.rb
@@ -1,7 +1,7 @@
 component "rubygem-fast_gettext" do |pkg, settings, platform|
   instance_eval File.read('configs/components/base-rubygem.rb')
-  pkg.version "1.1.0"
-  pkg.md5sum "fc0597bd4d84b749c579cc39c7ceda0f"
+  pkg.version "1.1.2"
+  pkg.md5sum "df5a2462d0e1d2e5d49bb233b841f4d6"
   pkg.url "https://rubygems.org/downloads/fast_gettext-#{pkg.get_version}.gem"
   pkg.mirror "#{settings[:buildsources_url]}/fast_gettext-#{pkg.get_version}.gem"
 
