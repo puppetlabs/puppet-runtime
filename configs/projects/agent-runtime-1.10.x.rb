34545ac9ce611c086e7c7ce24704ac286bdce277
Casey Williams <caseywilliams@users.noreply.github.com>
2018-02-19 19:12:38 -0800

    (maint) Add rubygem-ffi version 1.9.18

--- configs/projects/agent-runtime-1.10.x.rb
+++ configs/projects/agent-runtime-1.10.x.rb
@@ -3,6 +3,7 @@ project 'agent-runtime-1.10.x' do |proj|
 
   proj.setting :augeas_version, '1.4.0'
   proj.setting :rubygem_fast_gettext_version, '1.1.0'
+  proj.setting :rubygem_ffi_version, '1.9.14'
 
   # Common agent settings:
   instance_eval File.read('configs/projects/base-agent-runtime.rb')
