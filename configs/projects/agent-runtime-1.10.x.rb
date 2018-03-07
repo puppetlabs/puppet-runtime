afda876d681dd31fe301a7aa644d01d2db092ddb
Casey Williams <caseywilliams@users.noreply.github.com>
2018-03-07 12:52:48 -0800

    (PA-1833) Rely on local settings when building puppet-agent runtimes
    Previously, vanagon's `inherit_settings` feature was used to import
    build settings from puppet-agent for puppet-agent runtime projects. This
    commit migrates relevant settings from puppet-agent to puppet-runtime so
    that agent project builds do not rely on external resources.

--- configs/projects/agent-runtime-1.10.x.rb
+++ configs/projects/agent-runtime-1.10.x.rb
@@ -1,6 +1,5 @@
 project 'agent-runtime-1.10.x' do |proj|
-  proj.inherit_settings 'puppet-agent', 'git://github.com/puppetlabs/puppet-agent', '1.10.x'
-
+  proj.setting :ruby_version, '2.1.9'
   proj.setting :augeas_version, '1.4.0'
   proj.setting :rubygem_fast_gettext_version, '1.1.0'
   proj.setting :rubygem_ffi_version, '1.9.14'
@@ -8,6 +7,4 @@ project 'agent-runtime-1.10.x' do |proj|
 
   # Common agent settings:
   instance_eval File.read(File.join(File.dirname(__FILE__), 'base-agent-runtime.rb'))
-
-  proj.version proj.settings[:package_version]
 end
--- configs/projects/agent-runtime-master.rb
+++ configs/projects/agent-runtime-master.rb
@@ -1,12 +1,9 @@
 project 'agent-runtime-master' do |proj|
-  proj.inherit_settings 'puppet-agent', 'git://github.com/puppetlabs/puppet-agent', 'master'
-
+  proj.setting :ruby_version, '2.4.3'
   proj.setting :augeas_version, '1.10.1'
 
   # Common agent settings:
   instance_eval File.read(File.join(File.dirname(__FILE__), 'base-agent-runtime.rb'))
-
-  proj.version proj.settings[:package_version]
 
   # Dependencies specific to the master branch
   proj.component 'rubygem-multi_json'
