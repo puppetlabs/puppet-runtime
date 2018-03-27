e9df9a588e2b5aa349e0f111025f3a7ac62d5d8a
Scott McClellan <scott.mcclellan@puppetlabs.com>
2018-03-27 11:38:00 -0500

    (PA-1886) Remove gettext-setup gem (not branches 5.3, 5.5, 1.10)
    Since this gem is no longer used at runtime, it should be removed as a
    dependency. It cannot be removed in versions 5.3 and 5.5, so those
    will continue to provide the gettext-setup component.

--- configs/projects/agent-runtime-1.10.x.rb
+++ configs/projects/agent-runtime-1.10.x.rb
@@ -4,6 +4,7 @@ project 'agent-runtime-1.10.x' do |proj|
   proj.setting :rubygem_fast_gettext_version, '1.1.0'
   proj.setting :rubygem_ffi_version, '1.9.14'
   proj.setting :ruby_stomp_version, '1.3.3'
+  proj.component 'rubygem-gettext-setup'
 
   # Common agent settings:
   instance_eval File.read(File.join(File.dirname(__FILE__), 'base-agent-runtime.rb'))
--- configs/projects/agent-runtime-5.3.x.rb
+++ configs/projects/agent-runtime-5.3.x.rb
@@ -1,6 +1,7 @@
 project 'agent-runtime-5.3.x' do |proj|
   proj.setting :ruby_version, '2.4.3'
   proj.setting :augeas_version, '1.8.1'
+  proj.component 'rubygem-gettext-setup'
 
   # Common agent settings:
   instance_eval File.read(File.join(File.dirname(__FILE__), 'base-agent-runtime.rb'))
