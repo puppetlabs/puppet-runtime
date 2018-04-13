6270b9533223f9cdbfb1e78b8c794b00c440e40f
Branan Riley <branan@puppet.com>
2018-04-13 14:48:10 -0700

    (maint) Do not specify gem_vendor_dir in base runtime
    Agents 1.10. and 5.3.x should not have this setting, as that path does
    not exist in those package versions.

--- configs/projects/agent-runtime-5.5.x.rb
+++ configs/projects/agent-runtime-5.5.x.rb
@@ -5,6 +5,9 @@ project 'agent-runtime-5.5.x' do |proj|
   # Common agent settings:
   instance_eval File.read(File.join(File.dirname(__FILE__), 'base-agent-runtime.rb'))
 
+  # Directory for gems shared by puppet and puppetserver
+  proj.setting(:puppet_gem_vendor_dir, File.join(proj.libdir, "ruby", "vendor_gems"))
+
   # Dependencies specific to the 5.5.x branch
   proj.component 'rubygem-gettext-setup'
   proj.component 'rubygem-multi_json'
