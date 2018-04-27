7fb7f38695fb3d9d563537bb2fe248f769ea861b
Jorie Tappa <jorie@jorietappa.com>
2018-04-27 12:34:56 -0500

    Update semantic-puppet to latest 1.0.x version

--- configs/components/rubygem-semantic_puppet.rb
+++ configs/components/rubygem-semantic_puppet.rb
@@ -1,8 +1,8 @@
 component "rubygem-semantic_puppet" do |pkg, settings, platform|
   instance_eval File.read('configs/components/_base-rubygem.rb')
 
-  pkg.version "0.1.2"
-  pkg.md5sum "192ae7729997cb5d5364f64b99b13121"
+  pkg.version "1.0.2"
+  pkg.md5sum "48f4a060e6004604e2f46716bfeabcdc"
   pkg.url "https://rubygems.org/downloads/semantic_puppet-#{pkg.get_version}.gem"
   pkg.mirror "#{settings[:buildsources_url]}/semantic_puppet-#{pkg.get_version}.gem"
 
