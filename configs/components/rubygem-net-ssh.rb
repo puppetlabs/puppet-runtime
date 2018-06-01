4cb312a535406bb43b6472caea0b8cda9ed6af66
Casey Williams <caseywilliams@users.noreply.github.com>
2018-06-01 12:18:19 -0700

    (PA-2069) Offer net-ssh 4.1.0 for agent
    Fixes a regression: net-ssh 4.1.0 is restored to the puppet-agent 1.10.x
    and 5.5.x projects. net-ssh 4.2.0 remains the default version for other
    projects.

--- configs/components/rubygem-net-ssh.rb
+++ configs/components/rubygem-net-ssh.rb
@@ -1,8 +1,19 @@
 component "rubygem-net-ssh" do |pkg, settings, platform|
+  # Projects may define a :rubygem_net_ssh_version setting, or we use 4.2.0 by default:
+  version = settings[:rubygem_net_ssh_version] || '4.2.0'
+  pkg.version version
+
+  case version
+  when "4.2.0"
+    pkg.md5sum "fec5b151d84110b95ec0056017804491"
+  when "4.1.0"
+    pkg.md5sum "6af1ff8c42a07b11203058c9b74cbaef"
+  else
+    raise "rubygem-net-ssh version #{version} has not been configured; Cannot continue."
+  end
+
   instance_eval File.read('configs/components/_base-rubygem.rb')
 
-  pkg.version "4.2.0"
-  pkg.md5sum "fec5b151d84110b95ec0056017804491"
   pkg.url "https://rubygems.org/downloads/net-ssh-#{pkg.get_version}.gem"
   pkg.mirror "#{settings[:buildsources_url]}/net-ssh-#{pkg.get_version}.gem"
 
