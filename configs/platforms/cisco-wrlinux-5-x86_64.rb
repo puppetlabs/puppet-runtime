94a538d295cb83cce752f843b5c0f3782ea1b49f
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2018-08-02 11:59:24 -0700

    (PA-2024) Update netdev/f25 to get build_requires from platform files
    This commit updates the netdev and fedora 25 platforms to use platform files
    to specify build requirements.

--- configs/platforms/cisco-wrlinux-5-x86_64.rb
+++ configs/platforms/cisco-wrlinux-5-x86_64.rb
@@ -5,6 +5,15 @@ platform "cisco-wrlinux-5-x86_64" do |plat|
   # The development environment provided by Cisco includes the build dependencies
   # such as autoconf automake createrepo rsync gcc make rpm-build rpm-libs yum-utils
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/cisco-wrlinux/5/pl-build-tools-cisco-wrlinux-5.repo"
+  packages = [
+    "make",
+    "pkgconfig",
+    "pl-cmake",
+    "pl-gcc",
+    "readline-devel",
+    "zlib-devel",
+  ]
+  plat.provision_with("yum install -y --nogpgcheck  #{packages.join(' ')}")
   plat.install_build_dependencies_with "yum install -y"
   plat.vmpooler_template "cisco-wrlinux-5-x86_64"
 end
