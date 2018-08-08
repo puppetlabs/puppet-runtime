af0e8ba8dc7aec99a6f4d235e1996ae596a06198
Casey Williams <caseywilliams@users.noreply.github.com>
2018-08-08 09:45:56 -0700

    (PA-1824) Update config & patches for Ruby 2.5.1 on Windows
    - Port socket compat patch from 2.4.4 to 2.5.1
    - Update 2.5.1 rbconfig for Windows
    - Rename generated cmd scripts to bat

--- configs/components/ruby-2.5.1.rb
+++ configs/components/ruby-2.5.1.rb
@@ -95,9 +95,6 @@ component 'ruby-2.5.1' do |pkg, settings, platform|
   end
 
   if platform.is_windows?
-    pkg.apply_patch "#{base}/windows_fixup_generated_batch_files.patch"
-    pkg.apply_patch "#{base}/update_rbinstall_for_windows.patch"
-    pkg.apply_patch "#{base}/PA-1124_add_nano_server_com_support-8feb9779182bd4285f3881029fe850dac188c1ac.patch"
     pkg.apply_patch "#{base}/windows_socket_compat_error.patch"
   end
 
