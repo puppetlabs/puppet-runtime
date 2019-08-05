ea50548211d21b2e462ed5e458fd00a0f14c805a
donoghuc <cas.donoghue@gmail.com>
2019-08-05 13:55:12 -0700

    (BOLT-1504) Update bolt dependencies
    This commit updates the gems shipped in bolt and bolt-server system packages.

--- configs/components/rubygem-aws-sdk-core.rb
+++ configs/components/rubygem-aws-sdk-core.rb
@@ -1,6 +1,6 @@
 component "rubygem-aws-sdk-core" do |pkg, settings, platform|
-  pkg.version "3.59.0"
-  pkg.md5sum "2574a5c354d515e37e0eec35ed98dda4"
+  pkg.version "3.62.0"
+  pkg.md5sum "fc018e40d7e0d53fdd5c522c4735fb85"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
--- configs/components/rubygem-puppet-resource_api.rb
+++ configs/components/rubygem-puppet-resource_api.rb
@@ -1,6 +1,6 @@
 component 'rubygem-puppet-resource_api' do |pkg, settings, platform|
-  pkg.version '1.8.4'
-  pkg.md5sum 'bc43e38b0a90638c959994f0223744f6'
+  pkg.version '1.8.6'
+  pkg.md5sum 'f07ab551529b968230e6b8de07ae84d7'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
