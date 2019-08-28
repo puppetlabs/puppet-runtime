1abe3e78c8873cf8de166a89450648dc33c94240
Maggie Dreyer <maggie@puppet.com>
2019-08-28 08:57:54 -0700

    Revert "(PA-2864) Set GEM_HOME for concurrent"
    The `concurrent-ruby` gem has native extensions, so we need to keep
    installing two separate versions of it, one for MRI and one for JRuby.

--- configs/components/rubygem-concurrent-ruby.rb
+++ configs/components/rubygem-concurrent-ruby.rb
@@ -3,6 +3,4 @@ component 'rubygem-concurrent-ruby' do |pkg, settings, platform|
   pkg.md5sum '4409c2d6925d8448cb34a947eacaa29b'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
-
-  pkg.environment "GEM_HOME", (settings[:puppet_gem_vendor_dir] || settings[:gem_home])
 end
