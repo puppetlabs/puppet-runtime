3278ad8d211436c027d3be4dfd2eefef79d31cf6
donoghuc <cas.donoghue@gmail.com>
2019-09-27 07:20:56 -0700

    (packaging) Update gettext_setup gem accross shared runtimes
    This commit updates the `gettext_setup` gem which is shared by multiple runtime projects.

--- configs/components/rubygem-gettext-setup.rb
+++ configs/components/rubygem-gettext-setup.rb
@@ -1,6 +1,6 @@
 component "rubygem-gettext-setup" do |pkg, settings, platform|
-  pkg.version "0.30"
-  pkg.md5sum "b7de0e7af0f56ddc55c88435ee95fd47"
+  pkg.version "0.31"
+  pkg.md5sum "529706bf23b9c796d1ccad790764c41e"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
