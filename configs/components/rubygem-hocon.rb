e1d2fee27fcb5968eed2798867ba9c79a9d6c288
gimmy <gheorghe.popescu@puppet.com>
2019-10-25 12:57:58 +0300

    (maint) bump hocon gem to 1.2.6

--- configs/components/rubygem-hocon.rb
+++ configs/components/rubygem-hocon.rb
@@ -1,6 +1,6 @@
 component "rubygem-hocon" do |pkg, settings, platform|
-  pkg.version "1.2.5"
-  pkg.md5sum "e7821d3a731ab617320ccfa4f67f886b"
+  pkg.version "1.2.6"
+  pkg.md5sum "938b38e4029f024745cc9767ff0d94f4"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 
