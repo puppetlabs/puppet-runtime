b9e10219d16683b022a840c02b3072a5fa49d05e
donoghuc <cas.donoghue@gmail.com>
2019-11-05 07:32:27 -0800

    (packaging) Replace erubis with erubi for bolt/bolt-server runtime
    Due to sonos scan results, remove the erubis winrm/winrm-fs dependency and replace with erubi for bolt and pe-bolt-server runtime projects.

--- configs/components/rubygem-erubis.rb
+++
@@ -1,6 +0,0 @@
-component 'rubygem-erubis' do |pkg, settings, platform|
-  pkg.version '2.7.0'
-  pkg.md5sum 'cca3cf13ef951d1fc8c124d2fde52565'
-
-  instance_eval File.read('configs/components/_base-rubygem.rb')
-end
