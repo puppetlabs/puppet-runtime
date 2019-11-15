46324ccd6340b2ba8e7e88b0a1ea6a108ec07181
Tim Sharpe <tim@bombasticmonkey.com>
2019-11-15 13:50:22 +1100

    (PDK-1529) Update Git to 2.24.0

--- configs/components/git.rb
+++ configs/components/git.rb
@@ -1,12 +1,14 @@
 component "git" do |pkg, settings, platform|
   if platform.is_windows?
-    pkg.version "2.14.2.3"
-    pkg.md5sum "486fb9f0a73469b4634edecdff503091"
-    pkg.url "#{settings[:buildsources_url]}/MinGit-#{pkg.get_version}-64-bit.zip"
+    pkg.version "2.24.0.2"
+    pkg.md5sum "e7e3ee4c67c4697e44c2b7520a4087d5"
+    pkg.url "https://github.com/git-for-windows/git/releases/download/v#{pkg.get_version.split('.')[0..2].join('.')}.windows.#{pkg.get_version.split('.').last}/MinGit-#{pkg.get_version}-64-bit.zip"
+    pkg.mirror "#{settings[:buildsources_url]}/MinGit-#{pkg.get_version}-64-bit.zip"
   else
-    pkg.version "2.14.2"
-    pkg.md5sum "240b2e339029da98dd87ffbc44934278"
-    pkg.url "https://www.kernel.org/pub/software/scm/git/git-#{pkg.get_version}.tar.gz"
+    pkg.version "2.24.0"
+    pkg.md5sum "ed39361a3ae362c8af852d1a06992bc2"
+    pkg.url "https://mirrors.edge.kernel.org/pub/software/scm/git/git-#{pkg.get_version}.tar.gz"
+    pkg.mirror "#{settings[:buildsources_url]}/git-#{pkg.get_version}.tar.gz"
   end
 
   if platform.is_windows?
