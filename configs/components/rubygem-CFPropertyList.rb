4854aaeb4093dafc19feef62046e5d1360ea11f7
George Mrejea <george.mrejea@puppet.com>
2019-12-03 11:33:43 +0200

    (PA-2780) CFPropertyList gem is in the wrong directory

--- configs/components/rubygem-CFPropertyList.rb
+++ configs/components/rubygem-CFPropertyList.rb
@@ -3,4 +3,6 @@ component 'rubygem-CFPropertyList' do |pkg, settings, platform|
   pkg.md5sum 'ae4086185992f293ffab1641b83286a5'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
+
+  pkg.environment "GEM_HOME", (settings[:puppet_gem_vendor_dir] || settings[:gem_home])
 end
