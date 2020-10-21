a46f4bd385fd864a83b1a618d7676d86f6ef29a1
Lucy Wyman <wyman.lucy@gmail.com>
2020-10-21 06:48:19 -0700

    (maint) Bump puppet to 6.19
    Bumps the puppet gem to the newly released 6.19 version

--- configs/components/rubygem-puppet.rb
+++ configs/components/rubygem-puppet.rb
@@ -1,6 +1,6 @@
 component 'rubygem-puppet' do |pkg, settings, platform|
-  pkg.version "6.18.0"
-  pkg.md5sum 'bb3455dc67f65785192e0765527177e8'
+  pkg.version '6.19.0'
+  pkg.md5sum 'f2b4f1c523bbfdf5632a741d5657af11'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
