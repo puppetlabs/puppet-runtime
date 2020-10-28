9f02f34ec391776262f024afd8c11142bbc49711
donoghuc <cas.donoghue@gmail.com>
2020-10-28 08:45:46 -0700

    (PE-30417) Make bolt-server runtimes for 2019.8.x and main

--- configs/projects/pe-bolt-server-runtime-2019.8.x.rb
+++ configs/projects/pe-bolt-server-runtime-2019.8.x.rb
@@ -1,6 +1,5 @@
-project 'pe-bolt-server-runtime-2019.1.x' do |proj|
-  proj.setting(:pe_version, '2019.1')
+project 'pe-bolt-server-runtime-2019.8.x' do |proj|
+  proj.setting(:pe_version, 'master')
 
   instance_eval File.read(File.join(File.dirname(__FILE__), '_shared-pe-bolt-server.rb'))
 end
-
--- configs/projects/pe-bolt-server-runtime-main.rb
+++ configs/projects/pe-bolt-server-runtime-main.rb
@@ -1,6 +1,5 @@
-project 'pe-bolt-server-runtime-master' do |proj|
-  proj.setting(:pe_version, 'master')
+project 'pe-bolt-server-runtime-main' do |proj|
+  proj.setting(:pe_version, 'main')
 
   instance_eval File.read(File.join(File.dirname(__FILE__), '_shared-pe-bolt-server.rb'))
 end
-
