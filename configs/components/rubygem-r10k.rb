b8880f78ce0f3dbecb2721d7bfd81de18d7ff667
Tony Vu <vu.tony@gmail.com>
2020-11-13 15:18:03 -0800

    (RK-347) Bump r10k version for bolt

--- configs/components/rubygem-r10k.rb
+++ configs/components/rubygem-r10k.rb
@@ -1,6 +1,6 @@
 component 'rubygem-r10k' do |pkg, settings, platform|
-  pkg.version '3.5.2'
-  pkg.md5sum '14625f78d0157380a0050e2acb5ed7a8'
+  pkg.version '3.7.0'
+  pkg.md5sum 'd1e02ba57654feb81eb60b593cf87301'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
