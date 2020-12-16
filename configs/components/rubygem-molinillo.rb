e81de0db716d1990c87d0a2fbdec2d8cfb071c52
Lucy Wyman <wyman.lucy@gmail.com>
2020-12-16 14:49:02 -0800

    Revert "(maint) Bump shared rubygems for bolt, pe-bolt-server"

--- configs/components/rubygem-molinillo.rb
+++ configs/components/rubygem-molinillo.rb
@@ -1,6 +1,6 @@
 component "rubygem-molinillo" do |pkg, settings, platform|
-  pkg.version "0.7.0"
-  pkg.md5sum "3da77c3c3b5d604aa654b29390f25cb0"
+  pkg.version "0.6.6"
+  pkg.md5sum "f0f9d1fbb7277e3e4ae6c4be81fc0a92"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
