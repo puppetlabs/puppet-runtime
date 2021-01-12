811c9e83856852726f478f83592780042de1a2f9
Gabriel Nagy <gabrielnagy@me.com>
2021-01-12 19:49:11 +0200

    (maint) Bump semantic_puppet to 1.0.3

--- configs/components/rubygem-semantic_puppet.rb
+++ configs/components/rubygem-semantic_puppet.rb
@@ -1,13 +1,13 @@
 component "rubygem-semantic_puppet" do |pkg, settings, platform|
-  # Projects may define a :rubygem_semantic_puppet_version setting, or we use 1.0.2 by default
-  version = settings[:rubygem_semantic_puppet_version] || '1.0.2'
+  # Projects may define a :rubygem_semantic_puppet_version setting, or we use 1.0.3 by default
+  version = settings[:rubygem_semantic_puppet_version] || '1.0.3'
   pkg.version version
 
   case version
   when '0.1.2'
     pkg.md5sum '192ae7729997cb5d5364f64b99b13121'
-  when '1.0.2'
-    pkg.md5sum "48f4a060e6004604e2f46716bfeabcdc"
+  when '1.0.3'
+    pkg.sha256sum "3b83bff3bed1188d603947184c0e3ee4443543e3e832edccc7127b6209bb292b"
   else
     raise "rubygem-semantic_puppet version #{version} has not been configured; Cannot continue."
   end
