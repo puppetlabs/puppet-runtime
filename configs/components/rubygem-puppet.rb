708864cc3d989d631dc12ec18d22f049371e08db
Lucy Wyman <wyman.lucy@gmail.com>
2021-01-20 10:54:09 -0800

    (maint) Pull in Puppet 6.20.0 for Bolt
    Update the Puppet gem to 6.20.0 for Bolt and PE Bolt usage.

--- configs/components/rubygem-puppet.rb
+++ configs/components/rubygem-puppet.rb
@@ -1,6 +1,6 @@
 component 'rubygem-puppet' do |pkg, settings, platform|
-  pkg.version "6.19.1"
-  pkg.md5sum '8d68e677e6ba88e46eb00699dcd4930b'
+  pkg.version "6.20.0"
+  pkg.md5sum 'b1a6f244663f04075bafb1d36eede31c'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
