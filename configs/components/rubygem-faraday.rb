6b222ef956166a61e6c16756010dbe1995046c0b
Lucy Wyman <wyman.lucy@gmail.com>
2021-01-29 09:47:49 -0800

    (maint) Bump Bolt non-shared dependencies
    This bumps the versions for Bolt's gem dependencies that aren't shared
    with other projects to their latest possible version based on the
    current Bolt Gemfile.lock.

--- configs/components/rubygem-faraday.rb
+++ configs/components/rubygem-faraday.rb
@@ -1,6 +1,6 @@
 component 'rubygem-faraday' do |pkg, settings, platform|
-  pkg.version '0.14.0'
-  pkg.md5sum '9b11daa6e4a18bddeb7de9a4bc9e0315'
+  pkg.version '0.17.3'
+  pkg.md5sum 'aa0eb149651aa6185ca15b31edafb156'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
--- configs/components/rubygem-puppet-strings.rb
+++ configs/components/rubygem-puppet-strings.rb
@@ -1,6 +1,6 @@
 component 'rubygem-puppet-strings' do |pkg, settings, platform|
-  pkg.version '2.5.0'
-  pkg.md5sum 'e811763411ce0c36ffcb40aa5ca44212'
+  pkg.version '2.6.0'
+  pkg.md5sum '69557c69e8ed2675b57fc18e24d6e144'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
