4848bcbcd7f435eaa356c4446329b5e6f0bf4636
Ciprian Badescu <ciprian.badescu@puppet.com>
2021-04-22 15:41:15 +0300

    (PA-3535) update nspooler template name

--- configs/platforms/el-8-ppc64le.rb
+++ configs/platforms/el-8-ppc64le.rb
@@ -7,5 +7,5 @@ platform 'el-8-ppc64le' do |plat|
     patch swig libselinux-devel readline-devel zlib-devel systemtap-sdt-devel]
   plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
   plat.install_build_dependencies_with 'dnf install -y --allowerasing'
-  plat.vmpooler_template 'redhat-8-power9'
+  plat.vmpooler_template 'redhat-8-power8'
 end
