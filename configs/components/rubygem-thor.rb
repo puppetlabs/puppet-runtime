3bd91a07607b0489ed8f8c25332d9405077883d8
Lucy Wyman <wyman.lucy@gmail.com>
2021-04-27 09:00:26 -0700

    (maint) Bump Bolt shared dependencies
    This pulls in the latest versions of dependencies Bolt shares with the
    Puppet Agent projects.

--- configs/components/rubygem-thor.rb
+++ configs/components/rubygem-thor.rb
@@ -1,6 +1,6 @@
 component 'rubygem-thor' do |pkg, settings, platform|
-  pkg.version '1.0.1'
-  pkg.md5sum '052eb36fd3e522331af98449556991dc'
+  pkg.version '1.1.0'
+  pkg.md5sum '0d39b5be66778612f3233253e5b78ae6'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
