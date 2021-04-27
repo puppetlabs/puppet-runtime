da361b662c13b4c7f3749236b62175621258b60f
Lucy Wyman <wyman.lucy@gmail.com>
2021-04-27 07:08:47 -0700

    (maint) Bump Bolt and PE Bolt server dependencies
    This adds a new clause to allow projects to pull in Puppet 7.6.1 if they
    have the version specified in the project file, and bumps dependencies
    that are unique to Bolt and PE Bolt server. Shared dependency bumps are
    in a separate PR for review by teams that need them.

--- configs/components/rubygem-aws-partitions.rb
+++ configs/components/rubygem-aws-partitions.rb
@@ -1,6 +1,6 @@
 component "rubygem-aws-partitions" do |pkg, settings, platform|
-  pkg.version "1.431.0"
-  pkg.md5sum "cf93d88a599ea7ab48eeb5fb4dcb9f62"
+  pkg.version "1.447.0"
+  pkg.md5sum "5f79bd97f319761f41ff0c15ba566f55"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
--- configs/components/rubygem-bindata.rb
+++ configs/components/rubygem-bindata.rb
@@ -1,6 +1,6 @@
 component 'rubygem-bindata' do |pkg, settings, platform|
-  pkg.version '2.4.8'
-  pkg.md5sum '80a3ba1ffdbf020da0efb8c492c67a3a'
+  pkg.version '2.4.9'
+  pkg.md5sum 'a7b78a4d3a97cd97db5e24bd1e9345a2'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
