1eb99c2a12263e3e264d4df6e3cc083b61c65e6d
Josh Cooper <joshcooper@users.noreply.github.com>
2021-05-11 18:10:27 -0700

    (PA-3744) Build without site_ruby

--- configs/components/ruby-2.7.3.rb
+++ configs/components/ruby-2.7.3.rb
@@ -139,6 +139,7 @@ component 'ruby-2.7.3' do |pkg, settings, platform|
         --enable-bundled-libyaml \
         --disable-install-doc \
         --disable-install-rdoc \
+        --with-sitedir=no \
         #{settings[:host]} \
         #{special_flags}"
     ]
