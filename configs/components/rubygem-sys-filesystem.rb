827f2283ad481f9fdc37885a4ac0175bf2a186d8
Gabriel Nagy <gabriel.nagy@puppet.com>
2021-07-07 13:35:05 +0300

    (PA-3829) Bump sys-filesystem to 1.4.1
    Bump sys-filesystem to the latest available version to benefit from
    https://github.com/djberg96/sys-filesystem/commit/5c45ca06a0648b01ca0d5982f1dcd566bb188bdb
    
    Without this mountpoints don't resolve correctly on macOS M1.

--- configs/components/rubygem-sys-filesystem.rb
+++ configs/components/rubygem-sys-filesystem.rb
@@ -1,6 +1,6 @@
 component 'rubygem-sys-filesystem' do |pkg, settings, platform|
-  pkg.version '1.3.2'
-  pkg.md5sum '3cab54e4c93e483553f21d1080e5f2aa'
+  pkg.version '1.4.1'
+  pkg.sha256sum '4e907d2a18b0d1a5785896c39e9742ad1dc1e1f772f8154c20e7a1b5a41a9154'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
