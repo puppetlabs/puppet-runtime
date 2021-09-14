65fd77a9d59993068cae3f9ab89a6e9a5bfec876
Tom Beech <beech.tom@gmail.com>
2021-09-14 16:56:21 -0700

    (maint) Remove bootsnap/msgpack from bolt-runtime
    This removes the bootsnap and msgpack rubygem components from the
    bolt-runtime project, as they will no longer be used by Bolt.

--- configs/components/rubygem-bootsnap.rb
+++
@@ -1,8 +0,0 @@
-component 'rubygem-bootsnap' do |pkg, settings, platform|
-  pkg.version '1.8.0'
-  pkg.md5sum 'c5cad76ae2201d2f7da394f665df4dee'
-
-  pkg.build_requires 'rubygem-msgpack'
-  
-  instance_eval File.read('configs/components/_base-rubygem-native-extension.rb')
-end
