ed943f54aa8324ae9b486ad50b05ad941632e67c
Lucy Wyman <wyman.lucy@gmail.com>
2021-10-13 10:36:46 -0700

    (maint) Adopt Puppet 7.12 and bump Bolt dependencies
    This bumps Bolt's dependencies and picks up the new Puppet for Bolt.

--- configs/components/rubygem-aws-partitions.rb
+++ configs/components/rubygem-aws-partitions.rb
@@ -1,6 +1,6 @@
 component "rubygem-aws-partitions" do |pkg, settings, platform|
-  pkg.version "1.510.0"
-  pkg.md5sum "2b6b74a39fab464fd63690f961c8c5b9"
+  pkg.version "1.514.0"
+  pkg.md5sum "295dca1f9ebdfc2b0217b21f1be1c59e"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
--- configs/components/rubygem-aws-sdk-ec2.rb
+++ configs/components/rubygem-aws-sdk-ec2.rb
@@ -1,6 +1,6 @@
 component "rubygem-aws-sdk-ec2" do |pkg, settings, platform|
-  pkg.version "1.266.0"
-  pkg.md5sum "34b730c219852e2e20a0388fcf7615ba"
+  pkg.version "1.269.0"
+  pkg.md5sum "e6c7d2dff829193048872555219e2665"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
