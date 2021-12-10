7f7a4eaea96efd86722aef961573d96e6267ab37
Michael Hashizume <michael.hashizume@puppet.com>
2021-12-10 14:13:48 -0800

    (PA-4118) Adds Red Hat 9 support

---
+++ configs/platforms/el-9-x86_64.rb
@@ -0,0 +1,18 @@
+platform "el-9-x86_64" do |plat|
+  plat.inherit_from_default
+
+  packages = %w(
+    java-1.8.0-openjdk-devel
+    libsepol
+    libsepol-devel
+    libselinux-devel
+    pkgconfig 
+    readline-devel
+    rpmdevtools
+    swig
+    systemtap-sdt-devel
+    yum-utils
+    zlib-devel
+  )
+  plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
+end
