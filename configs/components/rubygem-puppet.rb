95ccdd8fd7a6797a4f840e7fc0f4d6be28d25b1a
rileynewton <95654936+rileynewton@users.noreply.github.com>
2022-01-04 11:48:31 -0800

    Bumped puppet 6.x version to 6.25.1

--- configs/components/rubygem-puppet.rb
+++ configs/components/rubygem-puppet.rb
@@ -1,14 +1,14 @@
 component 'rubygem-puppet' do |pkg, settings, platform|
   # Projects may define a :rubygem_puppet_version setting, or we use this
   # version by default
-  version = settings[:rubygem_puppet_version] || '6.25.0'
+  version = settings[:rubygem_puppet_version] || '6.25.1'
   pkg.version version
 
   case version
   when '7.13.1'
     pkg.md5sum 'd776b039312b67e00f1b3b64790efebe'
-  when '6.25.0'
-    pkg.md5sum 'c0b48b70a9faafd7449434b60c8428ec'
+  when '6.25.1'
+    pkg.md5sum '84c2c31b281152729476dfe04fd3a101'
   else
     raise "Invalid version #{version} for rubygem-puppet; Cannot continue."
   end
