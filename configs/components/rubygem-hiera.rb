6bf6be2b4b8a7f0e5409674ec2acb84fd3f1fdec
rileynewton <95654936+rileynewton@users.noreply.github.com>
2022-01-04 14:08:32 -0800

    (BOLT-1571) Add sys-filesystem gem dependency to Bolt runtime (#507)

--- configs/components/rubygem-hiera.rb
+++ configs/components/rubygem-hiera.rb
@@ -1,6 +1,6 @@
 component 'rubygem-hiera' do |pkg, settings, platform|
-  pkg.version '3.7.0'
-  pkg.md5sum '24fd0b1e4f449d09a81bf7688e0dcbde'
+  pkg.version '3.8.0'
+  pkg.md5sum 'a3f2e2fb2c31fdcdd18b2f881988a19e'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
