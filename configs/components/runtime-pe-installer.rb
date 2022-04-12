8aa29245ca525b38526d4dbf03418271844fc750
Nick Burgan <13688219+nmburgan@users.noreply.github.com>
2022-04-12 11:05:22 -0700

    Revert "(PE-33733) Include gems in installer-runtime that are present in agent-runtime"

--- configs/components/runtime-pe-installer.rb
+++ configs/components/runtime-pe-installer.rb
@@ -1,13 +1,31 @@
 # This component exists to link in the gcc runtime libraries.
 component "runtime-pe-installer" do |pkg, settings, platform|
   pkg.environment "PROJECT_SHORTNAME", "installer"
-  
-  if platform.name =~ /el-[67]|redhatfips-7|sles-12|ubuntu-(?:16.04|18.04-amd64)/
-    libbase = platform.architecture =~ /64/ ? 'lib64' : 'lib'
-    libdir = "/opt/pl-build-tools/#{libbase}"
-    pkg.add_source "file://resources/files/runtime/runtime.sh"
-    pkg.install do
-      "bash runtime.sh #{libdir}"
+
+  if platform.is_windows?
+    lib_type = platform.architecture == "x64" ? "seh" : "sjlj"
+    pkg.install_file "#{settings[:gcc_bindir]}/libgcc_s_#{lib_type}-1.dll", "#{settings[:bindir]}/libgcc_s_#{lib_type}-1.dll"
+
+    # zlib, gdbm, yaml-cpp and iconv are all runtime dependancies of ruby, and their libraries need to exist inside our vendored ruby
+    pkg.build_requires "pl-zlib-#{platform.architecture}"
+    pkg.install_file "#{settings[:tools_root]}/bin/zlib1.dll", "#{settings[:ruby_bindir]}/zlib1.dll"
+    pkg.install_file "#{settings[:tools_root]}/bin/libgdbm-4.dll", "#{settings[:ruby_bindir]}/libgdbm-4.dll"
+    pkg.install_file "#{settings[:tools_root]}/bin/libgdbm_compat-4.dll", "#{settings[:ruby_bindir]}/libgdbm_compat-4.dll"
+    pkg.install_file "#{settings[:tools_root]}/bin/libiconv-2.dll", "#{settings[:ruby_bindir]}/libiconv-2.dll"
+    pkg.install_file "#{settings[:tools_root]}/bin/libffi-6.dll", "#{settings[:ruby_bindir]}/libffi-6.dll"
+  elsif platform.is_macos?
+
+    # Do nothing
+
+  else # Linux and Solaris systems
+    if platform.name !~ /el-8-x86_64/
+      libbase = platform.architecture =~ /64/ ? 'lib64' : 'lib'
+      libdir = "/opt/pl-build-tools/#{libbase}"
+      pkg.add_source "file://resources/files/runtime/runtime.sh"
+      pkg.install do
+        "bash runtime.sh #{libdir}"
+      end
+    # Nothing needed for platforms without pl-build-tools
     end
   end
 end
