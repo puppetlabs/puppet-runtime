d871f302b103eb4bcaf67fe5dd8ef5917f53e8ee
Yehuda Katz <ykatz@cs.umd.edu>
2022-04-25 23:29:54 -0400

    (PA-4236) Add Ubuntu 22.04

---
+++ configs/platforms/ubuntu-22.04-amd64.rb
@@ -0,0 +1,16 @@
+platform "ubuntu-22.04-amd64" do |plat|
+  plat.inherit_from_default
+
+  packages = %w(
+    libbz2-dev
+    libreadline-dev
+    libselinux1-dev
+    openjdk-8-jre-headless
+    gcc
+    swig
+    systemtap-sdt-dev
+    zlib1g-dev
+  )
+  plat.provision_with "export DEBIAN_FRONTEND=noninteractive && apt-get update -qq && apt-get install -qy --no-install-recommends #{packages.join(' ')}"
+  plat.provision_with "curl https://artifactory.delivery.puppetlabs.net/artifactory/api/gpg/key/public | apt-key add -"
+end
