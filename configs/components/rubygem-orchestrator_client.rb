d89fc95c9892d8e2abc24db55a2e475414cc9683
donoghuc <cas.donoghue@gmail.com>
2022-05-18 11:10:47 -0700

    (BOLT-1583) Bump orchestrator_client gem
    Pull in bugfix for pcp timeout.

--- configs/components/rubygem-orchestrator_client.rb
+++ configs/components/rubygem-orchestrator_client.rb
@@ -1,6 +1,6 @@
 component 'rubygem-orchestrator_client' do |pkg, settings, platform|
-  pkg.version '0.5.2'
-  pkg.md5sum '97b3f0597a4fddf0ce88783c919748fc'
+  pkg.version '0.5.4'
+  pkg.md5sum 'f686c10f9dfd17b0296669fc5cbb91bd'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
