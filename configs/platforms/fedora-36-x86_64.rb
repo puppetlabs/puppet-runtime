e20c2f35107287164233a3858c9ef7410adb71e0
Michael Hashizume <michael.hashizume@puppet.com>
2022-07-26 16:33:16 -0700

    (PA-4445) Adds support for Fedora 36 (x86-64)

---
+++ configs/platforms/fedora-36-x86_64.rb
@@ -0,0 +1,11 @@
+platform 'fedora-36-x86_64' do |plat|
+  plat.inherit_from_default
+
+  packages = %w[
+    autoconf automake bzip2-devel gcc gcc-c++ libselinux-devel
+    libsepol libsepol-devel make cmake pkgconfig readline-devel
+    rpmdevtools rsync swig zlib-devel systemtap-sdt-devel
+    perl-lib perl-FindBin
+  ]
+  plat.provision_with("/usr/bin/dnf install -y --best --allowerasing #{packages.join(' ')}")
+end
