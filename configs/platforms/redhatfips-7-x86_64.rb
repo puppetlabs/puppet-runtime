0ba8606fef7c111d54da49d3b960bbafe46ad458
Josh Cooper <joshcooper@users.noreply.github.com>
2023-01-12 15:41:14 -0800

    (PA-4838) Push essential build packages for redhatfips-7-x86_64 into vanagon defaults
    See puppetlabs/vanagon#9b2df0c
    
    Updates el-7-x86_64 for readability

--- configs/platforms/redhatfips-7-x86_64.rb
+++ configs/platforms/redhatfips-7-x86_64.rb
@@ -2,17 +2,8 @@ platform "redhatfips-7-x86_64" do |plat|
   plat.inherit_from_default
 
   packages = %w(
-    java-1.8.0-openjdk-devel
-    libsepol
-    libsepol-devel
-    libselinux-devel
-    openssl-devel
-    pkgconfig
     pl-cmake
     pl-gcc
-    readline-devel
-    rpm-build
-    swig zlib-devel
   )
   plat.provision_with "yum install --assumeyes #{packages.join(' ')}"
 end
