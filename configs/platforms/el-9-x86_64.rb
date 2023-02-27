2a000297b1d7b46991cb1a4fe403f31b5e9a0da5
Gavin Patton <gavin.patton@puppet.com>
2023-02-27 16:57:20 +0000

    (CONT-550) Fix expat-devel install failure on RHEL 9
    Prior to this commit, puppet-runtime would faul to build due package installation failures. Specifically, the expat-devel package failed to install because it did not exist on our mirror. This commit makes use of a newly created repo with all of the packages needed to build puppet-runtime.

--- configs/platforms/el-9-x86_64.rb
+++ configs/platforms/el-9-x86_64.rb
@@ -15,4 +15,5 @@ platform "el-9-x86_64" do |plat|
     zlib-devel
   )
   plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
+  plat.provision_with("sed -i 's/beta-x86_64\/baseos\/x86_64/base/' /etc/yum.repos.d/localmirror-baseos.repo; sed -i 's/beta-x86_64\/appstream\/x86_64/appstream/' /etc/yum.repos.d/localmirror-appstream.repo")
 end
