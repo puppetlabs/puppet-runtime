540160979dcc6618ba412c26eaac85787a39a1aa
skyamgarp <130442619+skyamgarp@users.noreply.github.com>
2023-05-23 17:08:59 +0530

    (PA-5027)  Remove macOS 10.15 (x86_64) from the puppet-runtime repository

--- configs/platforms/osx-10.15-x86_64.rb
+++
@@ -1,9 +0,0 @@
-platform 'osx-10.15-x86_64' do |plat|
-  plat.inherit_from_default
-  packages = %w(
-    cmake
-    pkg-config
-    yaml-cpp
-  )
-  plat.provision_with "su test -c '/usr/local/bin/brew install #{packages.join(' ')}'"
-end
