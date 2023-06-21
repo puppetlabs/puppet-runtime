bb037dc201374bde3c17fbd4e293db15a15a2622
Amit Karsale <amit.karsale@perforce.com>
2023-06-21 12:45:02 +0530

    (PA-4592) added macos-13-x86_64 platform

---
+++ configs/platforms/osx-13-x86_64.rb
@@ -0,0 +1,8 @@
+platform 'osx-13-x86_64' do |plat|
+  plat.inherit_from_default
+
+  packages = %w[cmake pkg-config yaml-cpp]
+  plat.provision_with "su test -c '/usr/local/bin/brew install #{packages.join(' ')}'"
+
+  plat.output_dir File.join('apple', '13', 'PC1', 'x86_64')
+end
