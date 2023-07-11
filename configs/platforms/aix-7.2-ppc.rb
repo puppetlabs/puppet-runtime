a46d6e1d33ca748de66f76c915df625cb1e1ded2
Josh Cooper <joshcooper@users.noreply.github.com>
2023-07-10 22:51:44 -0700

    (PA-5645) Use yum update on AIX
    Whenver a fileset is installed via installp, e.g. openssl.base, IBM recommends
    running updtvpkg so that RPM packages which depend on it, build correctly.
    
    And after installing rpm.rte, yum prints that an update is required. This seems
    to resolve package dependencies issues between yum/python/libcurl and cmake.

--- configs/platforms/aix-7.2-ppc.rb
+++ configs/platforms/aix-7.2-ppc.rb
@@ -6,24 +6,35 @@ platform "aix-7.2-ppc" do |plat|
   plat.servicetype "aix"
   plat.tar "/opt/freeware/bin/tar"
 
-  plat.provision_with %[
+  plat.provision_with %(
 curl -O https://artifactory.delivery.puppetlabs.net/artifactory/generic__buildsources/openssl-1.1.2.2000.tar.Z;
 uncompress openssl-1.1.2.2000.tar.Z;
 tar xvf openssl-1.1.2.2000.tar;
 cd openssl-1.1.2.2000 && /usr/sbin/installp -acgwXY -d $PWD openssl.base;
-curl --output yum.sh https://artifactory.delivery.puppetlabs.net/artifactory/generic__buildsources/buildsources/aix-yum.sh && sh yum.sh]
+curl --output yum.sh https://artifactory.delivery.puppetlabs.net/artifactory/generic__buildsources/buildsources/aix-yum.sh && sh yum.sh)
+
+  # Since we updated openssl.base, run updtvpkg so that RPM packages that we install
+  # later build against the new openssl.
+  # See https://www.ibm.com/support/pages/understanding-aix-virtual-rpm-package-rpmrte
+  plat.provision_with 'updtvpkg'
 
   # After installing yum, but before yum installing packages, point yum to artifactory
   # AIX sed doesn't support in-place replacement, so download GNU sed and use that
-  plat.provision_with %[
+  plat.provision_with %(
 rpm -Uvh https://artifactory.delivery.puppetlabs.net/artifactory/rpm__remote_aix_linux_toolbox/RPMS/ppc/sed/sed-4.1.1-1.aix5.1.ppc.rpm;
-/opt/freeware/bin/sed -i 's|https://anonymous:anonymous@public.dhe.ibm.com/aix/freeSoftware/aixtoolbox/RPMS|https://artifactory.delivery.puppetlabs.net/artifactory/rpm__remote_aix_linux_toolbox/RPMS|' /opt/freeware/etc/yum/yum.conf]
+/opt/freeware/bin/sed -i 's|https://anonymous:anonymous@public.dhe.ibm.com/aix/freeSoftware/aixtoolbox/RPMS|https://artifactory.delivery.puppetlabs.net/artifactory/rpm__remote_aix_linux_toolbox/RPMS|' /opt/freeware/etc/yum/yum.conf)
+
+  # yum.sh downloads rpm.rte containing several base packages including curl
+  # 7.51. However, that version isn't compatible with cmake. If we update curl
+  # specifically, then yum/python/libcurl will be in an inconsistent state. So
+  # run `yum update` to update to newer versions while maintaining compatible
+  # versions.
+  plat.provision_with 'yum update --assumeyes --skip-broken'
 
   packages = %w(
     autoconf
     cmake
     coreutils
-    curl-7.86.0
     gawk
     gcc
     gcc-c++
