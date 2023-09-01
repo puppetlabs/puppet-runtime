ba83a9089a33dde77af6bcf0b5eeb28d020d539b
Maqsood Ahmad <maqsood.ulde@gmail.com>
2023-09-01 17:10:49 +0530

    (FACT-3175) Remove sys-filesystem monkey patch
    Bump the sys-filesystem gem in puppet-runtime
    as fix was submitted upstream and merged.

--- configs/components/rubygem-sys-filesystem.rb
+++ configs/components/rubygem-sys-filesystem.rb
@@ -1,8 +1,6 @@
 component 'rubygem-sys-filesystem' do |pkg, settings, platform|
-  pkg.version '1.4.1'
-  pkg.sha256sum '4e907d2a18b0d1a5785896c39e9742ad1dc1e1f772f8154c20e7a1b5a41a9154'
+  pkg.version '1.4.3'
+  pkg.sha256sum '390919de89822ad6d3ba3daf694d720be9d83ed95cdf7adf54d4573c98b17421'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
-
-  pkg.apply_patch "resources/patches/rubygem-sys-filesystem/linux-statvfs.patch", destination: "#{settings[:gem_home]}/gems/sys-filesystem-#{pkg.get_version}", after: "install"
 end
