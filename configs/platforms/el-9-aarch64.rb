72d5385ff8299d7283fc5303af148b58b5a2ada4
Amit Karsale <amit.karsale@perforce.com>
2023-09-13 15:09:01 +0530

    (PA-5733) Enable puppet-runtime builds for rhel9

---
+++ configs/platforms/el-9-aarch64.rb
@@ -0,0 +1,17 @@
+platform 'el-9-aarch64' do |plat|
+  plat.inherit_from_default
+
+  packages = %w(
+    perl-Getopt-Long 
+    java-1.8.0-openjdk-devel
+    patch 
+    swig 
+    libselinux-devel 
+    readline-devel 
+    zlib-devel 
+    systemtap-sdt-devel
+  )
+  plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
+  plat.install_build_dependencies_with "dnf install -y --allowerasing "
+  plat.vmpooler_template "redhat-9-arm64"
+end
