a6f80f8775d79f50ff063bb2bc3af50cd19f2929
donoghuc <cas.donoghue@gmail.com>
2023-10-10 11:30:25 -0700

    (PE-36966) Use puppet-strings 3.x sereies for LTS bolt-server
    The puppet-strings 4 series requires ruby 2.7. While the agent version ships with 2.7 the Jruby in the LTS
    is more akin to the ruby 2.6 spec. This makes the puppet-strings version break when run under Jruby. For the LTS
    we pin back to the 3 series.

--- configs/components/rubygem-puppet-strings.rb
+++ configs/components/rubygem-puppet-strings.rb
@@ -1,7 +1,18 @@
 component 'rubygem-puppet-strings' do |pkg, settings, platform|
-  pkg.version '4.1.0'
-  pkg.md5sum 'd8259d8a9144757f4e78f118b59e2681'
+  # 2021.7.x LTS has a jruby that does not work with puppet-strings
+  # version 4.y. The 4 series requires ruby >= 2.7
+  # Default to 4.x sereies but allow it to be configuratble by project.
+  version = settings[:rubygem_puppet_strings_version] || '4.1.0'
+  pkg.version(version)
 
+  case version
+  when '3.0.1'
+    pkg.md5sum '7c9a8936509a0434c39975a75197472c'
+  when '4.1.0'
+    pkg.md5sum 'd8259d8a9144757f4e78f118b59e2681'
+  else
+    raise "Invalid version #{version} for rubygem-puppet-strings; Cannot continue."
+  end
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
 
