34c902af5e2160245c6c50189fd274782d382cc7
Saurabh Pandit <saurabh.pandit@perforce.com>
2023-10-30 19:40:02 +0530

    [PA-5567] : Removed extra packages from x86_64 & aarch64 platform files

--- configs/platforms/amazon-2023-aarch64.rb
+++ configs/platforms/amazon-2023-aarch64.rb
@@ -1,18 +1,20 @@
+# rubocop:disable Naming/FileName
+# frozen_string_literal: true
+
 platform 'amazon-2023-aarch64' do |plat|
   plat.inherit_from_default
 
-  packages = %w(
-    perl
-    perl-Getopt-Long
-    java-1.8.0-amazon-corretto-devel
-    patch
-    swig
-    libselinux-devel
+  packages = %w[
+    perl-FindBin
+    perl-lib
     readline-devel
-    zlib-devel
     systemtap-sdt-devel
-  )
-  plat.provision_with("dnf install -y --allowerasing  #{packages.join(' ')}")
-  plat.install_build_dependencies_with "dnf install -y --allowerasing "
-  plat.vmpooler_template "amazon-2023-arm64"
+    zlib-devel
+  ]
+
+  plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
+  plat.install_build_dependencies_with 'dnf install -y --allowerasing'
+  plat.vmpooler_template 'amazon-2023-arm64'
 end
+
+# rubocop:enable Naming/FileName
--- configs/platforms/amazon-2023-x86_64.rb
+++ configs/platforms/amazon-2023-x86_64.rb
@@ -1,38 +1,20 @@
-platform "amazon-2023-x86_64" do |plat|
-  plat.servicedir "/usr/lib/systemd/system"
-  plat.defaultdir "/etc/sysconfig"
-  plat.servicetype "systemd"
+# rubocop:disable Naming/FileName
+# frozen_string_literal: true
 
-  packages = %w(
-    gcc
-    gcc-c++
-    autoconf
-    automake
-    createrepo
-    rsync
-    cmake
-    make
-    rpm-libs
-    rpm-build
-    rpm-sign
-    libtool
-    libarchive
-    java-1.8.0-amazon-corretto-devel
-    libsepol
-    libsepol-devel
-    libselinux-devel
-    pkgconfig
-    readline-devel
-    rpmdevtools
-    swig
-    systemtap-sdt-devel
-    yum-utils
-    zlib-devel
+platform 'amazon-2023-x86_64' do |plat|
+  plat.inherit_from_default
+
+  packages = %w[
     perl-FindBin
     perl-lib
-  )
+    readline-devel
+    systemtap-sdt-devel
+    zlib-devel
+  ]
 
   plat.provision_with "dnf install -y --allowerasing #{packages.join(' ')}"
-  plat.install_build_dependencies_with "dnf install -y --allowerasing "
-  plat.vmpooler_template "amazon-2023-x86_64"
+  plat.install_build_dependencies_with 'dnf install -y --allowerasing'
+  plat.vmpooler_template 'amazon-2023-x86_64'
 end
+
+# rubocop:enable Naming/FileName
