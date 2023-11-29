d84b4d1d922d1335fe74d1e7fe423433fb1d08c8
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2023-11-29 06:53:25 -0600

    (maint) remove vanagon overrides

--- configs/platforms/debian-11-amd64.rb
+++ configs/platforms/debian-11-amd64.rb
@@ -1,15 +1,5 @@
 platform "debian-11-amd64" do |plat|
-  # Delete the 6 lines below when a vanagon with Debian 11 support is released
-  plat.servicedir "/lib/systemd/system"
-  plat.defaultdir "/etc/default"
-  plat.servicetype "systemd"
-  plat.codename "bullseye"
-  plat.vmpooler_template "debian-11-x86_64"
-  plat.install_build_dependencies_with "DEBIAN_FRONTEND=noninteractive; apt-get install -qy --no-install-recommends "
-
-  # Uncomment these when a vanagon with Debian 11 support is released
-  # plat.inherit_from_default
-  # plat.clear_provisioning
+  plat.inherit_from_default
 
   packages = [
     'build-essential',
