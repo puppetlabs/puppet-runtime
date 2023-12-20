f201ef92c5c7b9930acc337c1e8f515e979b959c
Josh Cooper <joshcooper@users.noreply.github.com>
2023-12-19 17:25:59 -0800

    (PA-5960) Use concurrent-ruby 1.2.2 for all runtimes
    Versions prior to 1.2.x have a memory leak in JRuby when storing thread local
    state. Move everything to 1.2.2 and drop the old versions.
    
    This also switches to SHA256 for this gem and raises if a project requests an
    unknown version.

--- configs/components/rubygem-concurrent-ruby.rb
+++ configs/components/rubygem-concurrent-ruby.rb
@@ -1,14 +1,13 @@
 component 'rubygem-concurrent-ruby' do |pkg, settings, platform|
   # Projects may define a :rubygem_concurrent_ruby_version setting
-  version = settings[:rubygem_concurrent_ruby_version] || '1.1.10'
+  version = settings[:rubygem_concurrent_ruby_version] || '1.2.2'
+  pkg.version version
 
   case version
-  when '1.1.9'
-    pkg.version '1.1.9'
-    pkg.md5sum '417a23cac840f6ea8bdd0841429c3c19'
-  when '1.1.10'
-    pkg.version '1.1.10'
-    pkg.md5sum '4588a61d5af26e9ee12e9b8babc1b755'
+  when '1.2.2'
+    pkg.sha256sum '3879119b8b75e3b62616acc256c64a134d0b0a7a9a3fcba5a233025bcde22c4f'
+  else
+    raise "rubygem-concurrent-ruby #{version} has not been configured; Cannot continue."
   end
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
