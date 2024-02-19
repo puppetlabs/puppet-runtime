e832cd481c85109d94d2fe5fd5fe0f1e168b1b40
Shubham Shinde <shubham.shinde@perforce.com>
2024-02-19 19:18:36 +0530

    (PA-5999) Use pl-autotools instead of autoconf and automake for SLES-11
     - For Puppet8, we use ruby3 which requires autoconf to be >= 2.67, but SLES-11 supports only upto 2.63. So, we use pl-autotools for both puppet7 and puppet8 for consistency.

--- configs/platforms/sles-11-x86_64.rb
+++ configs/platforms/sles-11-x86_64.rb
@@ -6,11 +6,10 @@ platform "sles-11-x86_64" do |plat|
   plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/x86_64/pl-build-tools-sles-11-x86_64.repo"
   packages = [
     "aaa_base",
-    "autoconf",
-    "automake",
     "libbz2-devel",
     "make",
     "pkgconfig",
+    "pl-autotools",
     "pl-cmake",
     "pl-gcc",
     "readline-devel",
