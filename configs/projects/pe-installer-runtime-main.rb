a2828322db923e9086596bde46f3a790600e41df
donoghuc <cas.donoghue@gmail.com>
2024-03-27 09:12:57 -0700

    (PE-36494) Ship puppet gem with installer runtime

--- configs/projects/pe-installer-runtime-main.rb
+++ configs/projects/pe-installer-runtime-main.rb
@@ -8,5 +8,6 @@ project 'pe-installer-runtime-main' do |proj|
 
   # rubygem-net-ssh included in shared-agent-components
   proj.setting(:rubygem_net_ssh_version, '7.0.1')
+  proj.setting(:rubygem_puppet_version, '8.3.1')
   instance_eval File.read(File.join(File.dirname(__FILE__), '_shared-pe-installer-runtime.rb'))
 end
