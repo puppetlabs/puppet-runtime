176d4e045298055ee7c4cd9976a1aa2c80848d25
donoghuc <cas.donoghue@gmail.com>
2024-04-16 08:43:21 -0700

    (PE-38079) Fix typo in json-schema component
    Incorrectly specified the wrong version (the sha is correct)

--- configs/components/rubygem-json-schema.rb
+++ configs/components/rubygem-json-schema.rb
@@ -1,5 +1,5 @@
 component "rubygem-json-schema" do |pkg, settings, platform|
-  pkg.version "4.3.1"
+  pkg.version "4.3.0"
   pkg.sha256sum "ac35bfabf99eea2b8b45fbccbb714b399fbe7824c621fc985048a9c2e45d58d2"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
