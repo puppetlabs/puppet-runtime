c8062b03ff31a2f107aa34ee7154f213745c2ef0
donoghuc <cas.donoghue@gmail.com>
2024-04-16 10:20:11 -0700

    (PE-38079) Pin back semantic_puppet on cross-compiled platfomrms that use pl-ruby
    Copy pattern in ffi component for pinning back to older components on cross compiled platforms that dont use the shipped ruby to build gems.

--- configs/components/rubygem-semantic_puppet.rb
+++ configs/components/rubygem-semantic_puppet.rb
@@ -1,11 +1,18 @@
 component "rubygem-semantic_puppet" do |pkg, settings, platform|
   # Projects may define a :rubygem_semantic_puppet_version setting, or we use 1.0.4 by default
   version = settings[:rubygem_semantic_puppet_version] || '1.1.0'
+  if platform.is_cross_compiled? && platform.is_solaris?
+    # solaris 10/11 sparc are failing in agent-runtime-7.x installing semantic_puppet because the gem requires ruby >= 2.7.0
+    # when cross compiling on sparc, we use ancient pl-ruby or equivalen. For these older platforms, use older semantic_puppet
+    version = '1.0.4'
+  end
   pkg.version version
 
   case version
   when '0.1.2'
     pkg.md5sum '192ae7729997cb5d5364f64b99b13121'
+  when '1.0.4'
+    pkg.sha256sum "5d8380bf733c1552ef77e06a7c44a6d5b48def7d390ecf3bd71cad477f5ce13d"
   when '1.1.0'
     pkg.sha256sum "52d108d08e1a5d95c00343cb3a4936fb1deecff2be612ec39c9cb66be5a8b859"
   else
