45e13e6a453b2697bdfd07621034ec4ac7ce2636
donoghuc <cas.donoghue@gmail.com>
2024-04-16 16:51:18 -0700

    (maint) Fix typo in component name
    Luckily the filename itself covered this typo up (base64 gem still included). this commit fixes the typo for consistency.

--- configs/components/rubygem-base64.rb
+++ configs/components/rubygem-base64.rb
@@ -1,4 +1,4 @@
-component 'rubygem-bas64' do |pkg, settings, platform|
+component 'rubygem-base64' do |pkg, settings, platform|
   pkg.version '0.2.0'
   pkg.md5sum 'fe02f4347a5846f79f4d4615edababd3'
 
