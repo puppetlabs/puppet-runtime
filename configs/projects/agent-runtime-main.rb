0fcfda692deaaa1db8be3068ba0f5c9e0029cfd2
cthorn42 <christopher.thorn@puppet.com>
2024-05-02 12:24:03 -0700

    Revert "Add curl 8.7.1 and configure agent-runtime-main to use it"

--- configs/projects/agent-runtime-main.rb
+++ configs/projects/agent-runtime-main.rb
@@ -14,8 +14,6 @@ project 'agent-runtime-main' do |proj|
     proj.setting :augeas_version, '1.14.1'
   end
 
-  proj.setting :curl_version, '8.7.1'
-
   ########
   # Load shared agent settings
   ########
