8c485ffc61b37f32d3d8cb472845414bfdd7a1db
david22swan <david.swan@puppet.com>
2024-05-15 17:16:07 +0100

    (CAT-1643) Update childprocess pin
    Pin can be moved as PR against gem has been merged and released.

--- configs/components/rubygem-childprocess.rb
+++ configs/components/rubygem-childprocess.rb
@@ -1,6 +1,6 @@
 component "rubygem-childprocess" do |pkg, settings, platform|
-  pkg.version "4.1.0"
-  pkg.sha256sum '3616ce99ccb242361ce7f2b19bf9ff3e6bc1d98b927c7edc29af8ca617ba6cd3'
+  pkg.version "5.0.0"
+  pkg.sha256sum '0746b7ab1d6c68156e64a3767631d7124121516192c0492929a7f0af7310d835'
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
