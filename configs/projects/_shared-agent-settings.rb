7b20a00269ede841fad4d89673e1374f1a8cd424
skyamgarp <130442619+skyamgarp@users.noreply.github.com>
2024-08-06 20:34:08 +0530

    (PA-6383) Enable PIE for Ubuntu and Debian
    (PA-6383) Exclude ppc64 architecture
    
    (PA-6383) Fix Spaces
    
    (PA-6383) Exclude ppc64 arch
    
    (PA-6383) created compiler setting separately
    
    (PA-6383) Changes to runtime-bolt
    
    (PA-6383) Exclude EL platforms

--- configs/projects/_shared-agent-settings.rb
+++ configs/projects/_shared-agent-settings.rb
@@ -140,23 +140,8 @@ end
 proj.setting(:platform_triple, platform_triple)
 proj.setting(:host, host)
 
-# Define default CFLAGS and LDFLAGS for most platforms, and then
-# tweak or adjust them as needed.
-proj.setting(:cppflags, "-I#{proj.includedir} -I/opt/pl-build-tools/include")
-proj.setting(:cflags, "#{proj.cppflags}")
-proj.setting(:ldflags, "-L#{proj.libdir} -L/opt/pl-build-tools/lib -Wl,-rpath=#{proj.libdir}")
-
-# Platform specific overrides or settings, which may override the defaults
-
-# Harden Linux ELF binaries by compiling with PIE (Position Independent Executables) support,
-# stack canary and full RELRO.
-# We only do this on platforms that use their default OS toolchain since pl-gcc versions
-# are too old to support these flags.
-if platform.name =~ /sles-15|el-8|debian-10/ || platform.is_fedora?
-  proj.setting(:cppflags, "-I#{proj.includedir} -D_FORTIFY_SOURCE=2")
-  proj.setting(:cflags, '-fstack-protector-strong -fno-plt -O2')
-  proj.setting(:ldflags, "-L#{proj.libdir} -Wl,-rpath=#{proj.libdir},-z,relro,-z,now")
-end
+# Load default compiler settings
+instance_eval File.read('configs/projects/_shared-compiler-settings.rb')
 
 if ruby_version_x == "3"
   proj.setting(:openssl_version, '3.0')
