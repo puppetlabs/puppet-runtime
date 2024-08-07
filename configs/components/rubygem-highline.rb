1ee8f8f27ab336da36acce184948b62a316d25c8
alex501212 <alexthepatterson@hotmail.com>
2024-08-07 11:32:34 +0100

    removed 3.1.0 for highline

--- configs/components/rubygem-highline.rb
+++ configs/components/rubygem-highline.rb
@@ -5,8 +5,8 @@ component 'rubygem-highline' do |pkg, settings, _platform|
   case version
   when '2.1.0'
     pkg.md5sum '4209083bda845d47dcc05b7ab23f25fd'
-  when '3.1.0'
-    pkg.sha256sum '14183e4628f41fb4211766d0d38fb153f01f1ec39adfa83f5f7c6e6ebeb2b8eb'
+  when '3.0.1'
+    pkg.sha256sum 'ca18b218fd581b1fae832f89bfeaf2b34d3a93429c44fd4411042ffce286f009'
   else
     raise "rubygem-highline version #{version} has not been configured; Cannot continue."
   end
