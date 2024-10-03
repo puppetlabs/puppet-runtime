db0d8482e51e2e8d56bdc6f05f2632862b1bf8b5
Dana Doherty <danadoherty639@outlook.com>
2024-10-03 17:04:07 +0100

    (CAT-643) - Add puppet_forge gem and deps to pdk-runtime
    (CAT-643) - This commit adds 5.0.4 puppet forge gem as a component and a dependency to the PDK project
    
    dependencies of puppet-forge gem:
    faraday ~> 2.0 -> v2.12.0 added as component
    faraday-follow_redirects ~> 0.3.0 -> added as component
    minitar < 1.0.0 -> v0.9.0 already present in runtime
    semantic_puppet ~> 1.0 -> semantic puppet version 1.1.0 already present in runtime (default version)

---
+++ configs/components/rubygem-faraday-follow_redirects.rb
@@ -0,0 +1,6 @@
+component 'rubygem-faraday-follow_redirects' do |pkg, settings, platform|
+  pkg.version '0.3.0'
+  pkg.md5sum '75fa678fa40b54a94e51efc1600a6461'
+
+  instance_eval File.read('configs/components/_base-rubygem.rb')
+end
