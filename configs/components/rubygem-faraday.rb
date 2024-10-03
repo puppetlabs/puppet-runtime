b4e139f265c0d7ae0a6d8db8b390294110b1bf0a
Dana Doherty <danadoherty639@outlook.com>
2024-10-03 17:04:07 +0100

    (CAT-643) - Add puppet_forge

--- configs/components/rubygem-faraday.rb
+++ configs/components/rubygem-faraday.rb
@@ -1,6 +1,16 @@
 component 'rubygem-faraday' do |pkg, settings, platform|
-  pkg.version '1.10.3'
-  pkg.md5sum 'c7b56130721c0b055c071bec593e2446'
+  version = settings[:rubygem_faraday_version] || '1.10.3'
+
+  case version
+  when '1.10.3'
+    pkg.version '1.10.3'
+    pkg.md5sum 'c7b56130721c0b055c071bec593e2446'
+  when '2.12.0'
+    pkg.version '2.12.0'
+    pkg.md5sum 'c0248b00a32c46b64cd2a172c96409ec' 
+  else
+    raise "rubygem-faraday version #{version} is not supported"
+  end
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
--- configs/components/rubygem-puppet_forge.rb
+++ configs/components/rubygem-puppet_forge.rb
@@ -1,6 +1,17 @@
 component 'rubygem-puppet_forge' do |pkg, settings, platform|
-  pkg.version '3.2.0'
-  pkg.md5sum '501d5f9f742007504d0d60ce6cf0c27f'
+  version = settings[:rubygem_puppet_forge_version] || '3.2.0'
+  pkg.version version
+
+  case version
+  when '3.2.0'
+    pkg.version '3.2.0'
+    pkg.md5sum '501d5f9f742007504d0d60ce6cf0c27f'
+  when '5.0.4'
+    pkg.version '5.0.4'
+    pkg.md5sum '04a2ca2f027ed41d9142ced587b71bd7'
+  else 
+    raise "rubygem-puppet_forge version #{version} is not supported"
+  end
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
