e058fcff9ce507fa821918f4d43b910d9234d11a
Alex Patterson <56118464+alex501212@users.noreply.github.com>
2024-10-17 19:19:06 +0100

    updated components for bolt-runtime with ruby3/puppet8

--- configs/components/rubygem-aws-sigv4.rb
+++ configs/components/rubygem-aws-sigv4.rb
@@ -1,6 +1,6 @@
 component "rubygem-aws-sigv4" do |pkg, settings, platform|
-  pkg.version "1.9.1"
-  pkg.md5sum "3b9757f618ad2c151c73462cb5061a6f"
+  pkg.version "1.10.0"
+  pkg.md5sum "f9875621945414c4ee48113a3cb7076e"
 
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
