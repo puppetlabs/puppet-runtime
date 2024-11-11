a117162dd5a43327b98c16498d6a7381969498f9
alex501212 <alexthepatterson@hotmail.com>
2024-11-11 17:31:12 +0000

    updated gems and bolt runtime for r10k 4

--- configs/projects/bolt-runtime.rb
+++ configs/projects/bolt-runtime.rb
@@ -10,6 +10,10 @@ project 'bolt-runtime' do |proj|
   # TODO: Can runtime projects use these updated versions?
   proj.setting(:rubygem_deep_merge_version, '1.2.2')
   proj.setting(:rubygem_puppet_version, '8.8.1')
+  proj.setting(:rubygem_puppet_forge_version, '5.0.4')
+  proj.setting(:rubygem_faraday_version, '2.12.0')
+  proj.setting(:rubygem_faraday_net_http_version, '3.3.0')
+  proj.setting(:rubygem_faraday_net_http_persistent_version, '2.3.0')
 
   platform = proj.get_platform
 
@@ -158,7 +162,7 @@ project 'bolt-runtime' do |proj|
   proj.component 'rubygem-faraday-patron'
   proj.component 'rubygem-faraday-rack'
   proj.component 'rubygem-faraday-retry'
-  proj.component 'rubygem-faraday_middleware'
+  proj.component 'rubygem-faraday-follow_redirects'
   proj.component 'rubygem-ruby2_keywords'
 
   # Core dependencies
