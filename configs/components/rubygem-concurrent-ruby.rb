e6bf0df64e040993540e141f4ffdd35dabd00283
Sean P. McDonald <mcd.seanp@gmail.com>
2025-04-01 11:35:29 -0500

    (PE-40891) Update concurrent-ruby to latest: v1.3.5
    (Note: the ticket listed in this commit references bolt-server gem dep
    updates, and that work does require concurrent-ruby updates. This change
    applies everywhere however, since we might as well update gems where we
    can)
    
    Updates the concurrent-ruby gem everywhere to 1.3.5. Changes between
    v1.2.3 and v1.3.5 seem relatively minor: https://github.com/ruby-concurrency/concurrent-ruby/compare/v1.2.3...v1.3.5
    The only thing that appears to potentially be impactful is a change to
    align behavior in the java executor https://github.com/ruby-concurrency/concurrent-ruby/commit/8b9b0da4a37585ce5eb71516aca55e93bde39115
    which is something that should be a positive change.

--- configs/components/rubygem-concurrent-ruby.rb
+++ configs/components/rubygem-concurrent-ruby.rb
@@ -1,11 +1,11 @@
 component 'rubygem-concurrent-ruby' do |pkg, settings, platform|
   # Projects may define a :rubygem_concurrent_ruby_version setting
-  version = settings[:rubygem_concurrent_ruby_version] || '1.2.3'
+  version = settings[:rubygem_concurrent_ruby_version] || '1.3.5'
   pkg.version version
 
   case version
-  when '1.2.3'
-    pkg.sha256sum '82fdd3f8a0816e28d513e637bb2b90a45d7b982bdf4f3a0511722d2e495801e2'
+  when '1.3.5'
+    pkg.sha256sum '813b3e37aca6df2a21a3b9f1d497f8cbab24a2b94cab325bffe65ee0f6cbebc6'
   else
     raise "rubygem-concurrent-ruby #{version} has not been configured; Cannot continue."
   end
